2ebfd7b9fef6677caa0feb244c9ec847eaf3801d
James McKinney <james@slashpoundbang.com>
2009-02-27 23:52:56 -0500

    convert relative urls to absolute urls to avoid 404 errors in logs

--- fetch.py
+++ fetch.py
@@ -1,5 +1,5 @@
-import urllib2
-
+import urllib2, urlparse, html5lib
+from BeautifulSoup import BeautifulSoup
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
@@ -7,7 +7,14 @@ class Fetch(webapp.RequestHandler):
 	def get(self):
 		url = self.request.get('u')
 		response = urllib2.urlopen(url)
-		self.response.out.write(response.read())
+		# convert relative to absolute urls to avoid 404 errors in Google logs
+		parser = html5lib.HTMLParser(tree=html5lib.treebuilders.getTreeBuilder('beautifulsoup'))
+		soup = parser.parse(response)
+		for e in soup(src=True):
+			e['src'] = urlparse.urljoin(url, e['src'])
+		for e in soup(href=True):
+			e['href'] = urlparse.urljoin(url, e['href'])
+		self.response.out.write(str(soup))
 
 application = webapp.WSGIApplication([('/fetch', Fetch)], debug=True)
 
